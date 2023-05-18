3915f23ea8fe484631555f2e4036f748d9c4469c
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2023-05-18 10:37:53 +0500

    Update Customer Model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
-    protected $table="customers";
-    protected $primaryKey="customer_id";
+    protected $table = "customers";
+    protected $primaryKey = "customer_id";
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = ucwords($value);
+    }
 }
